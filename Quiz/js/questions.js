ddce60abf8282ff6011bdb0ddc7d79623d861079
sujeetghodke <115199135+sujeetghodke@users.noreply.github.com>
2024-01-29 21:12:49 +0530

    updated

---
+++ Quiz/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+   },
+    {
+    numb: 6,
+    question: "Javascript is an _______ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Javascript was first launched in which year ?",
+    answer: "1995",
+    options: [
+      "1993",
+      "1994",
+      "1995",
+      "1996"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The first version of HTML was written by ?",
+    answer: "Tim Berners-Lee",
+    options: [
+      "James Goslin",
+      "Tim Berners-Lee",
+      "Brendan Eich",
+      "Sujeet Ghodke"
+    ]
+  },
+  {
+    numb: 9,
+    question: "JavaScript was invented by ?",
+    answer: "Brendan Eich",
+    options: [
+      "James Goslin",
+      "Tim Berners-Lee",
+      "Brendan Eich",
+      "Vaishnavi Jagtap"
+    ]
+  },
+  {
+    numb: 10,
+    question: "HTML and CSS is used for ?",
+    answer: "Frontend Development",
+    options: [
+      "Frontend Development",
+      "Backend Development",
+      "FullStack Development",
+      "None of the Above"
+    ]
+  },
+];
